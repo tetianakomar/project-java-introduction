8d43b22d3ea4c2513927943a1d23672c5787546a
tetianakomar <komarmers@gmail.com>
2022-04-28 22:45:14 +0300

    add homework

---
+++ src/homework2/Main.java
@@ -0,0 +1,10 @@
+package homework2;
+
+public class Main {
+    public static void main(String[] args) {
+        float latitude = 48.26736f;
+        float longitude = 25.96206f;
+        System.out.println("Latitude Park Zhovtnevyi: " + latitude);
+        System.out.println("Longitude Park Zhovtnevyi: " + longitude);
+    }
+}

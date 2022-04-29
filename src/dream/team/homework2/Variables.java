b16aee29d0bfa03c7ca4cc68a7a8738c1615a743
tetianakomar <komarmers@gmail.com>
2022-04-29 22:39:19 +0300

    add homework

---
+++ src/dream/team/homework2/Variables.java
@@ -0,0 +1,43 @@
+package dream.team.homework2;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        byte myByte = 101;
+        System.out.println(myByte);
+
+        short myShort = 30000;
+        System.out.println(myShort);
+
+        int myInt = 3568999;
+        System.out.println(myInt);
+
+        long myLong = 100_000_000_000L;
+        System.out.println(myLong);
+
+        float myFloat = 45.890F;
+        System.out.println(myFloat);
+
+        double myDouble = 6789044.8576646;
+        System.out.println(myDouble);
+
+        boolean isValid = true;
+        System.out.println(isValid);
+
+        char myChar = '@';
+        System.out.println(myChar);
+
+        char myNumericChar = 2387;
+        System.out.println(myNumericChar);
+
+        char ch3 = '\u0054';
+        System.out.println(ch3);
+
+        String string = "test";
+        System.out.println(string);
+
+
+
+    }
+}

62e9f2271e0efdc9b8a68e024434705046bda476
tetianakomar <komarmers@gmail.com>
2022-05-08 00:00:39 +0300

    homework change

--- src/homework/lesson4/Airplanes.java
+++ src/homework/lesson4/Airplanes.java
@@ -14,23 +14,23 @@ public class Airplanes {
         int economySeatsNumber2 = 120;
         int economySeatsNumber3 = 180;
         int businessSeatsNumber3 = 28;
-        System.out.println("Type of airplane: " + printAirplaneInfo(nameOfAirplane1, typeOfAirplane1));
-        System.out.println("Type of airplane: " + printAirplaneInfo(nameOfAirplane2, typeOfAirplane2, economySeatsNumber2));
-        System.out.println("Type of airplane: " + printAirplaneInfo(nameOfAirplane3, typeOfAirplane3, economySeatsNumber3, businessSeatsNumber3));
+        System.out.println(printAirplaneInfo(nameOfAirplane1, typeOfAirplane1));
+        System.out.println(printAirplaneInfo(nameOfAirplane2, typeOfAirplane2, economySeatsNumber2));
+        System.out.println(printAirplaneInfo(nameOfAirplane3, typeOfAirplane3, economySeatsNumber3, businessSeatsNumber3));
 
     }
 
     public static String printAirplaneInfo(String nameOfAirplane1, String typeOfAirplane1) {
-        return nameOfAirplane1 + ", model: " + typeOfAirplane1;
+        return "Type of airplane: " + nameOfAirplane1 + ", model: " + typeOfAirplane1;
 
     }
 
     public static String printAirplaneInfo(String nameOfAirplane2, String typeOfAirplane2, int economySeatsNumber2) {
-        return nameOfAirplane2 + ", model: " + typeOfAirplane2 + ", number of economy class passengers: " + economySeatsNumber2;
+        return "Type of airplane: " + nameOfAirplane2 + ", model: " + typeOfAirplane2 + ", number of economy class passengers: " + economySeatsNumber2;
     }
 
     public static String printAirplaneInfo(String nameOfAirplane3, String typeOfAirplane3, int economySeatsNumber3, int businessSeatsNumber3) {
-        return nameOfAirplane3 + ", model: " + typeOfAirplane3 + ", number of economy class passengers: " + economySeatsNumber3 + ", number of business class passengers: " + businessSeatsNumber3;
+        return "Type of airplane: " + nameOfAirplane3 + ", model: " + typeOfAirplane3 + ", number of economy class passengers: " + economySeatsNumber3 + ", number of business class passengers: " + businessSeatsNumber3;
     }
 
 }
